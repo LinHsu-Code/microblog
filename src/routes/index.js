91eaf5073c18720f15697038724b59183a7724ef
Lin <dbllinxu@gmail.com>
2022-03-29 21:53:46 +0800

    feature: router test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,31 @@
-const router = require('koa-router')()
+const router = require("koa-router")();
 
-router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
+router.get("/", async (ctx, next) => {
+  await ctx.render("index", {
+    title: "Hello Koa 2!",
+  });
+});
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
-router.get('/json', async (ctx, next) => {
+router.get("/json", async (ctx, next) => {
   ctx.body = {
-    title: 'koa2 json'
-  }
-})
+    title: "koa2 json",
+  };
+});
 
-module.exports = router
+// router.get("/profile/:userName", async (ctx, next) => {
+//   const { userName } = ctx.params;
+//   ctx.body = {
+//     title: "this is a profile page",
+//     userName,
+//   };
+// });
+
+// router.get("/loadMore/:userName/:pageIndex", async (ctx, next) => {
+//   const { userName, pageIndex } = ctx.params;
+//   ctx.body = {
+//     title: "this is a loadMore API",
+//     userName,
+//     pageIndex,
+//   };
+// });
+module.exports = router;
