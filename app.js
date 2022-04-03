cb71c1b985bec16c596eee3ecac59b135790e74a
Lin <dbllinxu@gmail.com>
2022-04-03 12:27:17 +0800

    feat: session stored in redis

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const debug = require('debug')('http');
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
+const redis = require('redis');
+const connectRedis = require('connect-redis');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -18,7 +22,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+// enable this if you run behind a proxy (e.g. nginx)
+// app.set('trust proxy', 1);
+const RedisStore = connectRedis(session);
+// Configure redis client
+const redisClient = redis.createClient({
+  host: 'localhost',
+  port: 6379,
+});
+redisClient.on('error', (err) => {
+  debug(`Could not establish a connection with redis.${err}`);
+});
+redisClient.on('connect', () => {
+  debug('Connected to redis successfully');
+});
+// Configure session middleware
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: 'secret$%^134',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false, // if true only transmit cookie over https
+      httpOnly: false, // if true prevent client side JS from reading the cookie
+      maxAge: 1000 * 60 * 10, // session max age in miliseconds
+    },
+  })
+);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
